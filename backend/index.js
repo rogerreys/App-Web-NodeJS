535e4e7356e9072bba941a78c9a66d850365a88f
Roger Reyes <rreyes@cobiscorp.com>
2024-01-05 18:19:59 -0500

    Actualizacion index con cambios de api y rutas front

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser')
 
 const config = require("./config");
 const user = require("./api/components/user/network");
-const login = require("./api/components/login/network");
-const store = require("./api/components/store/network");
-var path = require("path");
+const login = require("./src/components/login/network");
+const store = require("./src/components/store/network");
+const api_store = require("./api/components/store/network");
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use("/user", user);
 app.use('/login', login);
 app.use('/store', store);
+app.use('/api/store', api_store);
 
 app.listen(config.api.PORT, () => {
     console.log(`Server is running on http://localhost:${config.api.PORT}`);
