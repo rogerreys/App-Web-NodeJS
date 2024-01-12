01f41806c9970b5bde39660e3c21b807d14fe29c
Angell50 <angel.vela@udla.edu.ec>
2024-01-11 21:49:00 -0500

    front de login

---
+++ backend/public/assts/js/store/login.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+function registroActivado() {
+    wrapper.classList.toggle('active');
+}
+function loginActivado() {
+    wrapper.classList.toggle('active');
+}
